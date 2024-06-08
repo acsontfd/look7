73d22cfe32df02dc08a463e91ca7ecc9300b4dc7
williamtan <william29570@gmail.com>
2024-06-08 18:30:00 +0800

    commit

--- app/src/main/java/com/example/look7/MainActivity.java
+++ app/src/main/java/com/example/look7/MainActivity.java
@@ -1,22 +1,41 @@
 package com.example.look7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button beginButton;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        beginButton = findViewById(R.id.beginButton);
 
-
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToChat();
+            }
+        });
+    }
+    private void navigateToChat() {
+        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
