5a7fa39769859d9a765bebaf7e6724d327b201e3
khyau16 <helloyaukimhui@gmail.com>
2024-06-08 20:29:11 +0800

    chat

--- app/src/main/java/com/example/look7/ChatActivity.java
+++ app/src/main/java/com/example/look7/ChatActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.os.CountDownTimer;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity{
     private TextView timerTextView;
@@ -17,16 +25,52 @@ public class ChatActivity extends AppCompatActivity{
     private boolean isTimerRunning = false;
     private long timeLeftInMillis = 120000; // 2 minute
 
+    RecyclerView recyclerView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_chat);
+            messageList = new ArrayList<>();
 
             timerTextView = findViewById(R.id.timerTextView);
 
             startTimer();
 
             updateTimerText();
+
+            recyclerView = findViewById(R.id.recycler_view);
+            messageEditText = findViewById(R.id.message_edit_text);
+            sendButton = findViewById(R.id.send_button);
+
+            messageAdapter = new MessageAdapter(messageList);
+            recyclerView.setAdapter(messageAdapter);
+            LinearLayoutManager llm = new LinearLayoutManager(this);
+            llm.setStackFromEnd(true);
+            recyclerView.setLayoutManager(llm);
+
+            sendButton.setOnClickListener(v -> {
+                String question = messageEditText.getText().toString().trim();
+                addToChat(question,Message.SENT_BY_ME);
+            });
+
+        }
+
+        void addToChat(String message, String sentBy){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageList.add(new Message(message, sentBy));
+                    messageAdapter.notifyDataSetChanged();
+                    recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+                }
+            });
+
         }
 
         private void startTimer() {
